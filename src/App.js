3a4b7939b6faf3eb274908baac4e67237603e531
VeniaminCobuz <95746858+VeniaminCobuz@users.noreply.github.com>
2023-04-11 20:47:10 +0300

    FIRST COMMIT

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Mesaj from "./components/Mesaj";
+import SixtySix from "./components/SixtySix";
+
+function App() {
+  return (<>
+    <Mesaj />
+    <SixtySix />
+  </>
+  );
+}
+
+export default App;
---
+++ src/components/SixtySix/index.js
@@ -0,0 +1,11 @@
+import './SixtySix.scss'
+
+import React from 'react'
+
+export default function SixtySix({ data }) {
+    return (
+        <div className={`sixtySix`}>
+            {/* <img className='gameImage' src={`${REACT_APP_RESOURCES}cartoane/game_photos/${data.hero}/${data.hero}_${data.photoType}.png`} alt='hero'></img> */}
+        </div>
+    )
+}
